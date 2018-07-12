b19913f49650817b8ddfea4ad53711884d6e5b3e
megashrieks <shrikanthbudya@gmail.com>
2018-07-12 18:45:26 +0530

    added roboto font.

--- src/components/Admin/BreadcrumbURL/BreadcrumbURL.js
+++ src/components/Admin/BreadcrumbURL/BreadcrumbURL.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import './BreadcrumbURL.css'
 class BreadcrumbURL extends Component {
 	render() {
-		return <div className="breadcrumb-url">BreadcrumbURL</div>;
+        return <div className="breadcrumb-url">
+            BreadcrumbURL
+        </div>;
 	}
 }
 export default BreadcrumbURL;
